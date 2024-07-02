20ddea9cdcd010d31a04ba70ccacd5e420460a5a
drbrodix <adamrigely@pm.me>
2024-07-02 19:42:04 +0200

    Initial Commit

---
+++ ListApp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ListApp
+//
+//  Created by Adam Rigely on 02.07.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
